5743995f8ce594779a0a2018cef059270c7ff8ae
DmitriyAgapov <prweb2012@gmail.com>
2023-05-15 17:41:41 +0500

    front env

--- Components/Scripts.tsx
+++ Components/Scripts.tsx
@@ -2,21 +2,21 @@ import Script from 'next/script';
 export default function DefaultScripts(){
 	return (
 		<>
-			<Script src="http://localhost:3000/assets/js/jquery-3.6.0.min.js" />
-			<Script src="http://localhost:3000/assets/js/bootstrap.bundle.min.js" />
-			<Script src="http://localhost:3000/assets/js/swiper-bundle.min.js" />
-			<Script src="http://localhost:3000/assets/js/counter.js" />
-			<Script src="http://localhost:3000/assets/js/gsap.min.js" />
-			<Script src="http://localhost:3000/assets/js/jquery-3.6.0.min.js" />
-			<Script src="http://localhost:3000/assets/js/ScrollTrigger.min.js"/>
-			<Script src="http://localhost:3000/assets/js/ScrollToPlugin.min.js"/>
-			<Script src="http://localhost:3000/assets/js/ScrollSmoother.min.js"/>
-			<Script src="http://localhost:3000/assets/js/SplitText.min.js"/>
-			<Script src="http://localhost:3000/assets/js/chroma.min.js"/>
-			<Script src="http://localhost:3000/assets/js/mixitup.min.js"/>
-			<Script src="http://localhost:3000/assets/js/vanilla-tilt.js"/>
-			<Script src="http://localhost:3000/assets/js/jquery.meanmenu.min.js"/>
-			<Script src="http://localhost:3000/assets/js/main.js"/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/jquery-3.6.0.min.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/bootstrap.bundle.min.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/swiper-bundle.min.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/counter.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/gsap.min.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/jquery-3.6.0.min.js`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/ScrollTrigger.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/ScrollToPlugin.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/ScrollSmoother.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/SplitText.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/chroma.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/mixitup.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/vanilla-tilt.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/jquery.meanmenu.min.j`}/>
+			<Script src={`${process.env.NODE_FRONT}/assets/js/main.j`}/>
 		</>
 	);
 }

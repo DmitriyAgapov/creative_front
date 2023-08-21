fa4ea2978712422eb6d4b61a4801b7a2ea1cc126
DmitriyAgapov <prweb2012@gmail.com>
2023-08-22 04:14:51 +0500

    jquery fixed

--- Components/ScriptsInner.tsx
+++ Components/ScriptsInner.tsx
@@ -9,7 +9,7 @@ export default function ScriptsInner(){
 		// componentDidMount() {}
 		const externalScript = document.createElement('script');
 		// @ts-ignore
-		externalScript.src = process.env.NODE_ENV === 'development' ? 'http://localhost:10001/assets/js/main.js' : `${process.env.NODE_FRONT}/assets/js/main.js`;
+		externalScript.src = process.env.NODE_ENV === 'development' ? 'http://localhost:10001/assets/js/main.js' : `https://theonebureau.design/assets/js/main.js`;
 		externalScript.async = true;
 		document.head.append(externalScript);
 
@@ -25,20 +25,20 @@ export default function ScriptsInner(){
 	}, []);
 
 	return <>
-		<Script strategy={'afterInteractive'} src={`${process.env.NODE_ENV === 'development' ?    'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/jquery-3.6.0.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/bootstrap.bundle.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/swiper-bundle.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/counter.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/gsap.min.js`}/>
-		{/*<Scr  strategy={'afterInteractive'}  ipt sr${process.env.NODE_ENV === 'development' ? 'http://localhost:10001/ : process.env.NODE_FRONT}:10001/assets/js/jquery-3.6.0.min.js`}/>*/}
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/ScrollTrigger.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/ScrollToPlugin.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/ScrollSmoother.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/SplitText.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/chroma.min.js`}/>
-		<Script  strategy={'afterInteractive'}    src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/mixitup.min.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/vanilla-tilt.js`}/>
-		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : process.env.NODE_FRONT}/assets/js/jquery.meanmenu.min.js`}/>
+		<Script strategy={'afterInteractive'}    src={`${process.env.NODE_ENV === 'development' ?    'http://localhost:10001' : 'https://theonebureau.design/assets/js/jquery-3.6.0.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/bootstrap.bundle.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/swiper-bundle.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/counter.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/gsap.min.js'}`}/>
+		{/*<Scr  strategy={'afterInteractive'}  ipt sr${process.env.NODE_ENV === 'development' ? 'http://localhost:10001/ : 'https://theonebureau.design}:10001/assets/js/jquery-3.6.0.min.js`}/>*/}
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/ScrollTrigger.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/ScrollToPlugin.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/ScrollSmoother.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/SplitText.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/chroma.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}    src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/mixitup.min.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/vanilla-tilt.js'}`}/>
+		<Script  strategy={'afterInteractive'}   src={`${process.env.NODE_ENV === 'development' ? 'http://localhost:10001' : 'https://theonebureau.design/assets/js/jquery.meanmenu.min.js'}`}/>
 
 
 	</>;

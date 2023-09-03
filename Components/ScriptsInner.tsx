89655f61f18a108931e0f4f1fadc79289a099ef4
DmitriyAgapov <93589129+DmitriyAgapov@users.noreply.github.com>
2023-09-04 00:10:32 +0500

    vercel

--- Components/ScriptsInner.tsx
+++ Components/ScriptsInner.tsx
@@ -1,44 +1,82 @@
-'use client'
-import Script from 'next/script';
-import { useEffect} from "react";
+"use client";
+import Script from "next/script";
+import { useEffect, useLayoutEffect } from "react";
 import { usePathname } from "next/navigation";
-export default function ScriptsInner(){
-	const pathname = usePathname()
+export default function ScriptsInner() {
+  const pathname = usePathname();
 
-	useEffect(() => {
-		// componentDidMount() {}
-		const externalScript = document.createElement('script');
-		// @ts-ignore
-		externalScript.src = process.env.NODE_ENV === 'development' ? 'http://localhost:10001/assets/js/main.js' : `https://theonebureau.design/assets/js/main.js`;
-		externalScript.async = true;
-		document.head.append(externalScript);
+  useLayoutEffect(() => {
+    // componentDidMount() {}
+    const externalScript = document.createElement("script");
+    // @ts-ignore
+    externalScript.src =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:10001/assets/js/main.js"
+        : `https://theonebureau.design/assets/js/main.js`;
+    externalScript.async = true;
+    document.head.append(externalScript);
 
-		// const inlineScript = document.createElement('script');
-		// inlineScript.innerHTML = '...';
-		// document.body.append(inlineScript);
+    // const inlineScript = document.createElement('script');
+    // inlineScript.innerHTML = '...';
+    // document.body.append(inlineScript);
 
-		// componentWillUnmount() {}
-		return () => {
-			externalScript.remove();
-			// inlineScript.remove();
-		};
-	}, []);
+    // componentWillUnmount() {}
+    return () => {
+      externalScript.remove();
+      // inlineScript.remove();
+    };
+  }, []);
 
-	return <>
-
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/bootstrap.bundle.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/swiper-bundle.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/counter.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/gsap.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/ScrollTrigger.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/ScrollToPlugin.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/ScrollSmoother.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/SplitText.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/chroma.min.js`}/>
-		<Script  strategy={'beforeInteractive'}    src={`https://theonebureau.design/assets/js/mixitup.min.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/vanilla-tilt.js`}/>
-		<Script  strategy={'beforeInteractive'}   src={`https://theonebureau.design/assets/js/jquery.meanmenu.min.js`}/>
-
-
-	</>;
+  return (
+    <>
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/bootstrap.bundle.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/swiper-bundle.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/counter.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/gsap.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/ScrollTrigger.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/ScrollToPlugin.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/ScrollSmoother.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/SplitText.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/chroma.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/mixitup.min.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/vanilla-tilt.js`}
+      />
+      <Script
+        strategy={"beforeInteractive"}
+        src={`https://theonebureau.design/assets/js/jquery.meanmenu.min.js`}
+      />
+    </>
+  );
 }
